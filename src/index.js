dce92f31f4e57e16d113e1f8955cc7315d5acd31
CrazyheadJake <jake.d.moleski@gmail.com>
2026-02-11 19:36:01 -0800

    Basic navbar has been setup with handlebars as the templating engine

--- src/index.js
+++ src/index.js
@@ -1,15 +1,50 @@
 const express = require('express')
-const path = require('path');
+const path = require('path')
+const hbs = require('express-handlebars')
 
-const app = express();
-const PORT = 5723
+const app = express()
+const PORT = 5173
 
 app.use(express.static(path.join(__dirname, '../public')));
+app.engine('hbs', hbs.engine({
+    extname: 'hbs',
+    defaultLayout: 'main',
+    layoutsDir: path.join(__dirname, '../views/layouts'),
+    partialsDir: path.join(__dirname, '../views/partials'),
+    helpers: {
+        navActive: (targetPage, currentPage) => {
+            console.log(`Comparing targetPage: ${targetPage} with activePage: ${currentPage}`);
+            return targetPage === currentPage ? 'active' : '';
+        }
+    }
+}))
 
-// app.get('/', (req, res) => {
-//     res.send('Hello World!')
-// })
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, '../views'));
 
+app.get('/', (req, res) => {
+    res.render('home', { currentPage: 'Home' })
+})
+
+app.get('/authors', (req, res) => {
+    res.render('authors', { currentPage: 'Authors' })
+})
+
+app.get('/books', (req, res) => {
+    res.render('books', { currentPage: 'Books' })
+})
+
+app.get('/members', (req, res) => {
+    res.render('members', { currentPage: 'Members' })
+})
+
+app.get('/genres', (req, res) => {
+    res.render('genres', { currentPage: 'Genres' })
+})
+
+app.get('/borrows', (req, res) => {
+    res.render('borrows', { currentPage: 'Borrows' })
+})
 app.listen(PORT, () =>
     console.log(`Server running on http://localhost:${PORT}`)
 )
