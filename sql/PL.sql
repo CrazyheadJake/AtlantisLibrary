b758c70f5a186e9a4ef1134cd711f4552ea5f6e2
minor33 <minorr@oregonstate.edu>
2026-03-02 21:50:07 -0800

    Simplified insert procedure to take in IDs

--- sql/PL.sql
+++ sql/PL.sql
@@ -1,21 +1,8 @@
 DROP PROCEDURE IF EXISTS insert_book;
 DELIMITER //
 
-CREATE PROCEDURE insert_book (IN pTitle VARCHAR(50), IN pAuthorFirstName VARCHAR(50), IN pAuthorLastName VARCHAR(50), IN pGenreName VARCHAR(50))
+CREATE PROCEDURE insert_book (IN pTitle VARCHAR(50), IN pAuthorID INT, IN pGenreCode VARCHAR(50))
 BEGIN
-    DECLARE pAuthorID INT;
-    DECLARE pGenreCode INT;
-
-    SELECT Authors.authorID
-    INTO pAuthorID
-    FROM Authors
-    WHERE Authors.firstName = pAuthorFirstName AND Authors.lastName = pAuthorLastName;
-
-    SELECT Genres.genreCode
-    INTO pGenreCode
-    FROM Genres
-    WHERE Genres.genreName = pGenreName;
-
     START TRANSACTION;
 
     INSERT INTO Books (title, authorID, genreCode) VALUES
