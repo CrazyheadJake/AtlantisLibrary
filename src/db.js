3d1e37bea162b05bbfbc349ad8043f95fa75b439
CrazyheadJake <jake.d.moleski@gmail.com>
2026-03-16 21:32:22 -0700

    Added comments to all of the js code

--- src/db.js
+++ src/db.js
@@ -1,5 +1,6 @@
 import { createPool } from 'mariadb'
 
+// Open up a connection pool to the MariaDB database
 const pool = createPool({
     host: 'classmysql.engr.oregonstate.edu',
     user: 'cs340_moleskij',
@@ -8,39 +9,47 @@ const pool = createPool({
     connectionLimit: 5
 });
 
+// Reset the database to just the original test data
 async function resetDb() {
     await pool.query('CALL reset_database();');
 }
 
+// Get authors for the authors page
 async function getAuthors() {
     const authors = await pool.query('SELECT * FROM Authors;');
     return authors;
 }
 
+// Get books as well as their respective authors and genres
 async function getBooks() {
     const books = await pool.query('SELECT b.bookID, b.title, a.firstName, a.lastName, g.genreName FROM Books AS b JOIN Authors AS a ON a.authorID=b.authorID JOIN Genres AS g ON g.genreCode=b.genreCode;');
     return books;
 }
 
+// Insert a book into the system
 async function insertBook(title, authorID, genreCode) {
     await pool.query('CALL insert_book(?, ?, ?);', [title, authorID, genreCode]);
 }
 
+// Get all members for the members page
 async function getMembers() {
     const members = await pool.query('SELECT * FROM Members;');
     return members;
 }
 
+// Get all genres for the genres page
 async function getGenres() {
     const genres = await pool.query('SELECT * FROM Genres;');
     return genres;
 }
 
+// Get all borrows for the borrows dropdown
 async function getBorrows() {
     const borrows = await pool.query('SELECT b.borrowID, m.firstName, m.lastName, b.startTime, b.dueTime FROM Borrows AS b JOIN Members AS m ON m.memberID=b.memberID;');
     return borrows;
 }
 
+// Get all borrows for the borrows update page
 async function getFullBorrows() {
     const borrows = await pool.query('SELECT * from Borrows;');
     const books = await pool.query('SELECT * FROM Books_Borrows;');
--- src/index.js
+++ src/index.js
@@ -4,14 +4,12 @@ import hbs from 'express-handlebars'
 import { pool, resetDb, getAuthors, getBooks, getGenres, getMembers, insertBook, getBorrows, getFullBorrows} from './db.js'
 import { fileURLToPath } from 'url';
 import { dirname } from 'path';
-import { error } from 'console';
-import e from 'express';
 
+// Set up the express app to use handlebars and run on a given port
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 const app = express()
 const PORT = 5172
-
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, '../public')))
 app.engine('hbs', hbs.engine({
@@ -25,19 +23,25 @@ app.engine('hbs', hbs.engine({
         }
     }
 }))
-
 app.set('view engine', 'hbs')
 app.set('views', path.join(__dirname, '../views'))
 
+/*
+Beginning of backend routes 
+*/
+
+// Home page
 app.get('/', (req, res) => {
     res.render('home', { currentPage: 'Home', success: req.query.success })
 })
 
+// Authors page
 app.get('/authors', async (req, res) => {
     const authors = await getAuthors()
     res.render('authors', { currentPage: 'Authors', authors, error: req.query.error, success: req.query.success })
 })
 
+// Books page
 app.get('/books', async (req, res) => {
     const [authors, books, genres] = await Promise.all([
         getAuthors(),
@@ -47,17 +51,21 @@ app.get('/books', async (req, res) => {
     res.render('books', { currentPage: 'Books', authors, books, genres, booksJSON: JSON.stringify(books), error: req.query.error, success: req.query.success })
 })
 
+// Members page
 app.get('/members', async (req, res) => {
     const members = await getMembers()
     res.render('members', { currentPage: 'Members', members, error: req.query.error, success: req.query.success })
 })
 
+// Genres page
 app.get('/genres', async (req, res) => {
     const genres = await getGenres()
     res.render('genres', { currentPage: 'Genres', genres, error: req.query.error, success: req.query.success })
 })
 
+// Borrows page
 app.get('/borrows', async (req, res) => {
+    // Get all of these simulatenously from the db rather than waiting one by one
     const [books, members, borrows, fullBorrows] = await Promise.all([
         getBooks(),
         getMembers(),
@@ -67,11 +75,13 @@ app.get('/borrows', async (req, res) => {
     res.render('borrows', { currentPage: 'Borrows', books, members, borrows, borrowsJSON: JSON.stringify(fullBorrows), error: req.query.error, success: req.query.success })
 })
 
+// Resets the database
 app.post('/reset', async (req, res) => {
     await resetDb()
     res.redirect('/?success=Database+reset+successfully')
 })
 
+// Insert a book
 app.post('/books', async (req, res) => {
     if (!req.body.title || !req.body.author || !req.body.genre) {
         res.redirect('/books?error=Please+fill+in+all+fields');
@@ -82,6 +92,7 @@ app.post('/books', async (req, res) => {
     res.redirect('/books?success=Book+inserted+successfully')
 })
 
+// Insert an author
 app.post('/authors', async (req, res) => {
     try {
         await pool.query('CALL insert_author(?, ?, ?);', [req.body.firstName, req.body.lastName, req.body.bio]);
@@ -92,13 +103,16 @@ app.post('/authors', async (req, res) => {
     }
 });
 
+// Insert a member
 app.post('/members', async (req, res) => {
     await pool.query('CALL insert_member(?, ?, ?);', [req.body.firstName, req.body.lastName, req.body.email]);
     res.redirect('/members?success=Member+inserted+successfully');
 });
 
+// Insert a genre
 app.post('/genres', async (req, res) => {
     try {
+        // Prevent empty string for a genre
         if (!req.body.genreName) {
             res.redirect('/genres?error=Please+fill+in+all+fields');
             return;
@@ -106,34 +120,37 @@ app.post('/genres', async (req, res) => {
         await pool.query('CALL insert_genre(?);', [req.body.genreName]);
     }
     catch (error) {
+        // If there was a duplicate, catch the error cleanly and return an error
         res.redirect('/genres?error=You cannot insert a duplicate genre');
         return;
     }
     res.redirect('/genres?success=Genre+inserted+successfully');
 });
 
+// Insert a new borrow
 app.post('/borrows/insert', async (req, res) => {
     let connection;
     try {
+        // Prevent a borrow with no books selected
+        if (!req.body.books) {
+            res.redirect('/borrows?error=Please+select+at+least+1+book');
+            return;
+        }
+        // Insert a new borrow with the corresponding member and start/end times
         connection = await pool.getConnection();
         await connection.query(
             'CALL insert_borrow(?, ?, ?, @borrowID);', 
             [req.body.member, req.body.startTime, req.body.dueTime]
         );
-        console.log("inserted borrow")
+        // Add books one by one for each selected book
         const rows = await connection.query('SELECT @borrowID AS borrowID;');
-        console.log("retrieved borrowID: ", rows[0].borrowID);
         const borrowID = rows[0].borrowID;
-        if (!req.body.books) {
-            res.redirect('/borrows?error=Please+select+at+least+1+book');
-            return;
-        }
         for (const bookID of req.body.books) {
-            console.log(`Adding bookID ${bookID} to borrowID ${borrowID}`);
             await pool.query('CALL add_book_to_borrow(?, ?);', [borrowID, bookID]);
         }
         res.redirect('/borrows?success=Borrow+inserted+successfully');
     } catch (err) {
+        // If fields are missing (such as member), return a user readable error
         console.log("Error inserting borrow: ", err);
         res.redirect('/borrows?error=Failed+to+insert+borrow.+Please+ensure+all+fields+are+filled+out.');
     } finally {
@@ -141,12 +158,14 @@ app.post('/borrows/insert', async (req, res) => {
     }
 });
 
+// Update a borrow with new information
 app.post('/borrows/update', async (req, res) => {
     const { editborrowID, editmember, editstartTime, editreturnTime, editdueTime} = req.body
+    // Update the member as well as the start, return, and due times
     await pool.query('CALL update_borrow(?, ?, ?, ?, ?);', [editborrowID, editmember, editstartTime, editreturnTime, editdueTime]);
 
+    // Clear all existing books in the borrow and then re-add all of the books selected by the user
     await pool.query('CALL clear_books_borrows(?);', [editborrowID]);
-
     let books = req.body.editbookName;
     if (!books) {
         books = [];
@@ -154,18 +173,19 @@ app.post('/borrows/update', async (req, res) => {
     else if (!Array.isArray(books)) {
         books = [books];
     }
-    
     for (const bookID of books) {
         await pool.query('CALL add_book_to_borrow(?, ?);', [editborrowID, bookID]);
     }
     res.redirect('/borrows?success=Borrow+updated+successfully');
 });
 
+// Delete an existing borrow
 app.post('/borrows/delete', async (req, res) => {
     await pool.query('CALL delete_borrow(?);', [req.body.borrow]);
     res.redirect('/borrows?success=Borrow+deleted+successfully');
 });
 
+// Start the app
 app.listen(PORT, () =>
     console.log(`Server running on http://localhost:${PORT}`)
 )
